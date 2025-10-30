71095cd6779712293e1a08ae4b15681ae4158c3a
kmt <kmt@kmt.local>
2025-10-30 13:28:23 -0400

    working, but cuts off the end

--- demo/main.gd
+++ demo/main.gd
@@ -7,22 +7,37 @@ extends Control
 var video : Video = Video.new()
 
 
-var video_path : String = "/Users/kmt/Desktop/gangwisch_bodybloxXR_short.mp4"
+var video_path : String = "/Users/kmt/Desktop/test2.mp4"
 var is_playing: bool = false
 
 var current_frame: int = 1
 var max_frame: int = 0;
+var audio_last_frame: int = 0
+var file_last_frame : int = 0
 var framerate: float = 0;
 var frame_time : float = 0
+
+var looping : bool = false
+
+var time_elapsed: float = 0
+
+var timeline_dragging : bool = false
 
 func _ready() -> void:
 	if FileAccess.file_exists(video_path) and video_path.ends_with(".mp4"):
 		video.open_video(video_path)
 		audio_player.stream = video.get_audio()
 		
-		max_frame = video.get_total_frame_nr()
+		
 		framerate = video.get_avg_framerate()
 		frame_time = 1.0 / framerate #in seconds
+		
+		file_last_frame = video.get_total_frame_nr()
+		audio_last_frame = audio_player.stream.get_length()*framerate
+		max_frame = file_last_frame#min(audio_last_frame, file_last_frame)
+		print(file_last_frame)
+		print(audio_last_frame)
+		print(max_frame)
 
 		timeline.min_value = 1
 		timeline.value = 1
@@ -33,26 +48,54 @@ func _ready() -> void:
 		print("No video file at path.")
 	
 func seek_frame(a_frame_nr: int)->void:
-	current_frame = a_frame_nr
+	current_frame = clampi(a_frame_nr, 1, max_frame)
 	texture_rect.texture.set_image(video.seek_frame(current_frame))
+	audio_player.seek(current_frame/framerate)
 	#print(current_frame)
-func _process(_delta: float) -> void:
-	if is_playing:
-		if current_frame >= max_frame:
-			pass
-		else:
-			texture_rect.texture.set_image(video.next_frame())
+	
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	
+	if is_playing and time_elapsed >= frame_time:
+			go_to_next_frame()
+
+func go_to_next_frame()->void:
+	while time_elapsed >= frame_time:
+		time_elapsed -= frame_time
+		current_frame += 1
+	
+	
+	if current_frame >= max_frame:
+		seek_frame(1)
+		if not looping:
+			audio_player.stop()
+			is_playing = false
+	else:
+		var image : Image = video.next_frame()
+		if not image.is_empty():
+			texture_rect.texture.set_image(image)
 			current_frame += 1
+		
+	if not timeline_dragging:
 		timeline.value = current_frame
 		frame_label.text = str(current_frame) + " / " + str(max_frame)
+
 func _on_play_pause_button_pressed() -> void:
 	
 	is_playing = !is_playing
-	audio_player.set_stream_paused(is_playing)
+	if is_playing:
+		audio_player.play()
+	else:
+		audio_player.set_stream_paused(true)
 
 
 func _on_timeline_drag_ended(value_changed: bool) -> void:
+	timeline_dragging = false
 	if !value_changed:
-		print("test")
 		return
 	seek_frame(int(timeline.value))
+	
+
+
+func _on_timeline_drag_started() -> void:
+	timeline_dragging = true
